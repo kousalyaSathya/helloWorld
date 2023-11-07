b216375d13c9196d7cc67373dfb6d966137f8762
user-kousalya <150127716+user-kousalya@users.noreply.github.com>
2023-11-07 17:06:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 import React from 'react';
-function App() {
-    return (
-        <h1> Hello World! </h1>
-    );
-}
-export default App;
+// function App() {
+//     // return (
+//     //     <h1> Hello World! </h1>
+//     // );
+// }
+// export default App;
 //this is a sample comment
 console.log('this is sample log')

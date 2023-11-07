33fbbf0cf03dbe60e26659a8f371a49f37e13e50
learning-lab <140042704+kousalyaSathya@users.noreply.github.com>
2023-11-07 17:32:00 +0530

    phase1-1stCommit (#1)
    Co-authored-by: Kousalya005 <kousalya.s@unifo.in>

---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+// this is the header of the webpage
+import React from 'react'
+
+function header() {
+    return (
+        <div>
+            <h1>Sonar cloud</h1>
+        </div>
+    )
+}
+
+export default header
